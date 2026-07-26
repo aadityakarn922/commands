5ecc866e343ca93a658cf1b1ffcebf65c63a359a
aadityakarn922 <aadityakarn922@gmail.com>
2026-07-26 10:55:16 +0545

    bshj

--- chown/practice.sh
+++ chown/practice.sh
@@ -5,6 +5,13 @@ echo "Current ownership:"
 ls -l
 
 echo
-echo"available users:"
+echo "available users:"
 cut -d: -f1 /etc/passwd
 
+echo "change owneer of notes.txt to postgres"
+
+sudo chown postgres notes.txt
+echo "change back to original owner"
+
+sudo chown aaditya notes.txt
+``
